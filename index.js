3aab286f88b91396236cb6d394bfdfb2d7d855a6
zoubaiba <lyliaaitmokhtar5@gmail.com>
2026-02-03 17:32:00 +0100

    zebi

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // renvoyer l’element HTML d’identifiant #game_selection :
 let message1 = document.querySelector("#message1");
 let game_selection = document.querySelector("#game_selection");
@@ -6,6 +8,9 @@ let game2 = document.querySelector("#game2");
 let restart_button1 = document.querySelector("#restart_button1");
 let restart_button2 = document.querySelector("#restart_button2");
 let try_button = document.querySelector("#try_button");
+
+let computer_number; // Pour le jeu 1 
+let min, max, proposition; // Pour le jeu 2 
 
 // afficher un texte dans l’´element message1
 message1.innerText = "un petit texte";
@@ -18,40 +23,47 @@ function show(element) {
 function hide(element) {
 element.style.display = "none";
 }
-function new_game(game_selection) { // fonction qui montre l’´ecran de s´election du mode de jeu
-    if (game_selection == "#game2") {
-        hide(game2);
-        show(game1);
-    } else if (game_selection == "#game1") {
-        hide(game1);
-        show(game2);
-    }
+function new_game() { // fonction qui montre l’´ecran de s´election du mode de jeu
+show(game_selection);
+hide(game1);
+hide(game2);
 }
 
 function start_player_guess_game() {
     new_game("#game1");
-    let computer_number = Math.random() * 100;
-    hide(game2);
+    computer_number = Math.random() * 100;
     hide(game_selection);
+    show(game1);
     hide(restart_button1);
-    let number = document.getElementById("player_input").value.parseInt();
 
+    player_input.value = "";
+    message1.innerText = "";
+}
+
+function game1_starter(){
+    
+    console.log("game1 starter")
+    let number = document.getElementById("player_input").value;
+    number = parseInt(number);
+    console.log("number" , number)
+    console.log("cmp number",computer_number)
     if (number == computer_number) {
-        show(message1);
+
         message1.innerText = "Bravo, vous avez trouv´";
+        show(restart_button1);
+
     } else if (number < computer_number) {
-        show(message1);
+        
         message1.innerText = "Non, c'est plus grand";
     } else if (number > computer_number) {
-        show(message1);
+    
         message1.innerText = "Non, c’est plus petit";
     }
     document.getElementById("player_input").value = "";
 }
+try_button.addEventListener("click", game1_starter);
 
 
-
-document.querySelector("#restart_button1").addEventListener("click", new_game("#game1"));
-document.querySelector("#try_button").addEventListener("click", start_player_guess_game);
-document.querySelector("#start_game1").addEventListener("click", new_game("#game1"));
-document.querySelector("#start_game2").addEventListener("click", new_game("#game2"));
+document.querySelector("#restart_button1").addEventListener("click", new_game);
+document.querySelector("#start_game1").addEventListener("click", start_player_guess_game);
+document.querySelector("#start_game2").addEventListener("click", new_game);
